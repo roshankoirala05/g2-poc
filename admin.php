4ea4ed2291b66ea1158539917aecb69b99ae4fe4
Roshan Koirala <roshankoirala05@gmail.com>
2017-01-31 18:40:18 +0000

    hey

--- admin.php
+++ admin.php
@@ -1,12 +1,7 @@
-<?php
 
-echo'
 <a href="monroe.php">Click to view all the people from monroe</a>
 
 <form method="post" action="index.php">
             
             <input type="submit" value="Exit"/>
         </form>
-';
-
-?>
