ce4cd572fe4ce9afab43da613cc14490542b699e
Raj Subedi <subedin@warhawks.ulm.edu>
2017-02-26 17:10:34 -0600

    Add files via upload
    Guys, I finished the Pin Map, Form (With validation), Admin login page........ I am still working touch pin functionality on the map. Most probably I will be working admin filter function tonight, but  I have to study for Operating system too so If you guys work on admin filter, it will be great. We are almost done.

---
+++ prototypefirst/workspace/Database.php
@@ -0,0 +1,108 @@
+<?php
+class Form
+{
+    public $firstname;
+     public $lastname;
+    private $cityName;
+    private $stateName;
+    private $zipCode;
+    private $countryName;
+    private $noInParty;
+    private $travelingFor;
+    private $howDidYouHear;
+    private $didYouStay;
+    private $email;
+
+public function __construct($name, $cityName, $stateName, $zipCode, $countryName, $noInParty, $travelingFor, $howDidYouHear, $didYouStay, $email=null )
+{
+
+   $this-> setFirstName($firstname);
+    $this-> setLastName($lastname);
+   $this-> setCityName($cityName);
+   $this-> setStateName($stateName);
+   $this-> setZipCode($zipCode);
+   $this-> setCountryName($countryName);
+   $this-> setNoInParty($noInParty);
+   $this-> setTravelingFor($travelingFor);
+   $this-> setHowDidYouHear($howDidYouHear);
+   $this-> setDidYouStay($didYouStay);
+   $this-> setEmail($email);
+
+}
+
+ public function getFirstName() { return $this->firstname; }
+  public function getLastName() { return $this->lastname; }
+ public function getCityName() { return $this->cityName; }
+ public function getStateName() { return $this->stateName; }
+ public function getZipCode() { return $this->zipCode; }
+ public function getCountryName() { return $this->countryName;}
+ public function getNumberInParty() { return $this->noInParty; }
+ public function getTravelingFor() { return $this->travelingFor; }
+ public function getHowDidYouHear() { return $this->howDidYouHear; }
+ public function getDidYouStay() { return $this->didYouStay; }
+ public function getEmail() { return $this->email; }
+
+
+public function setName ($name)
+{
+    $this->firstname=$lastname;
+}
+public function setLastName ($lastname)
+{
+    $this->lastname=$lastname;
+}
+
+public function setCityName ($cityName)
+{
+    $this->cityName=$cityName;
+}
+
+public function setStateName ($stateName)
+{
+    $this->stateName=$stateName;
+}
+
+public function setZipCode ($zipCode)
+{
+    $this->zipCode=$zipCode;
+}
+
+public function setCountryName ($countryName)
+{
+    $this->countryName=$countryName;
+}
+
+public function setNoInParty ($noInParty)
+{
+    $this->noInParty=$noInParty;
+}
+
+public function setTravelingFor ($travelingFor)
+{
+    $this->travelingFor=$travelingFor;
+}
+
+public function setHowDidYouHear ($howDidYouHear)
+{
+    $this->howDidYouHear=$howDidYouHear;
+}
+
+public function setDidYouStay ($didYouStay)
+{
+    $this->didYouStay=$didYouStay;
+}
+
+public function setEmail ($email)
+{
+    $this->email=$email;
+}
+
+public function _toString()
+{
+
+ return $this->name."<br/>". $this->cityName."<br/>". $this->countryName;
+
+}
+
+}
+?>
