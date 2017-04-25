7d538e5004f2edb4fd8cc05b3e6fba0d4b876606
Raj Subedi <newrajsubedi@gmail.com>
2017-04-25 06:21:40 +0000

    Again Here is the final templates for Admin login with session, forgot password form with ajax and create admin for master admin. Please change your admin database. I have added the Admin sql in this submission and you can easily change by Admin.sql file and you can see the chart_data.php and chart_sample.html for to create graph to analyze data

---
+++ chart_data.php
@@ -0,0 +1,36 @@
+<?php
+  include 'Database.php';
+  include 'DatabaseConnection.php';
+  $raj= new DatabaseConnection();
+
+//the SQL query to be executed
+$query = "SELECT State, COUNT( * ) AS Number
+FROM VISITOR
+GROUP BY State
+HAVING COUNT( * ) >1";
+
+
+//storing the result of the executed query
+$result = $raj->returnQuery($query);
+
+//initialize the array to store the processed data
+$jsonArray = array();
+
+//check if there is any data returned by the SQL Query
+if ($result->num_rows > 0) {
+  //Converting the results into an associative array
+  while($row = $result->fetch_assoc()) {
+    $jsonArrayItem = array();
+    $jsonArrayItem['label'] = $row['State'];
+    $jsonArrayItem['value'] = $row['Number'];
+    //append the above created object into the main array.
+    array_push($jsonArray, $jsonArrayItem);
+  }
+}
+
+
+//set the response content type as JSON
+header('Content-type: application/json');
+//output the return value of json encode using the echo function.
+echo json_encode($jsonArray);
+?>
