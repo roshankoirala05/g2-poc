567f92f4773851eceba535fe10ff98e75a38a0a1
Raj Subedi <newrajsubedi@gmail.com>
2017-05-01 17:07:00 +0000

    Greeting message fixed

--- js/addressAjax.js
+++ js/addressAjax.js
@@ -14,27 +14,42 @@ function myFunction() {
   success: function (data) {
    if (data["Country"] === "US") {
 
-    if (data['number'] > 3) {
-     wellcome = "You are the " + data['number'] + "th visitor from<br>" + data["City"] + ", " + data["State"] + ", " + data["Country"];
+    if (data['number'] <= 3) {
+     if (data['number'] == 1) {
+      wellcome = "You are the " + "1st" + " visitor from<br>" + data["City"] + ", " + data["State"] + ", " + data["Country"];
+     }
+     else if (data['number'] == 2) {
+      wellcome = "You are the " + "2nd" + " visitor from<br>" + data["City"] + ", " + data["State"] + ", " + data["Country"];
+     }
+     else {
+      wellcome = "You are the " + "3rd" + " visitor from<br>" + data["City"] + ", " + data["State"] + ", " + data["Country"];
+     }
 
-     document.getElementById("well").innerHTML = wellcome;
     }
     else {
-     wellcome = "You are the " + data['number'] + " visitor from<br>" + data["City"] + ", " + data["State"] + ", " + data["Country"];
+     wellcome = "You are the " + data['number'] + "th visitor from<br>" + data["City"] + ", " + data["State"] + ", " + data["Country"];
 
-     document.getElementById("well").innerHTML = wellcome;
     }
+    document.getElementById("well").innerHTML = wellcome;
    }
 
    else {
-    if (data['number'] > 3) {
-     wellcome = "You are the " + data["number"] + "th visitor from<br>" + data["Country"];
-     document.getElementById("well").innerHTML = wellcome;
+    if (data['number'] <= 3) {
+     if (data['number'] == 1) {
+      wellcome = "You are the " + "1st" + " visitor from<br>" + data["Country"];
+     }
+     else if (data['number'] == 2) {
+      wellcome = "You are the " + "2nd" + " visitor from<br>" + data["Country"];
+     }
+     else {
+      wellcome = "You are the " + "3rd" + " visitor from<br>" + data["Country"];
+     }
     }
     else {
      wellcome = "You are the " + data["number"] + " visitor from<br>" + data["Country"];
-     document.getElementById("well").innerHTML = wellcome;
+
     }
+    document.getElementById("well").innerHTML = wellcome;
    }
   },
   error: function (xhr, ajaxOptions, thrownError) {
